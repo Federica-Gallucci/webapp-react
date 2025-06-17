66a823b958b7dc1fa4c7c4a7ed9a69ed77e9c177
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-06-17 15:17:07 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import HomePage from "./pages/HomePage";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
